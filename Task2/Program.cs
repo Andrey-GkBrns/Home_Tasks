a3f02b7524aaf7e4fb0d47a8485d13be0e0c8351
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-02 19:13:13 +0300

    Задача2 выполнена

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа для сравнения двух чисел - большего из них или равного друг другу.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите другое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+Console.WriteLine($"Данное число {a} > {b}");
+else if (a < b)
+Console.WriteLine($"В этом случае {b} > {a}");
+else
+Console.WriteLine($"А эти числа равны: {a} = {b}");
