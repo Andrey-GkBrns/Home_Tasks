d51f6e61c23da3c160bde8d9b94029a392585b19
Andrey-GkBrns <Oran@Gutang.ru>
2023-02-03 09:48:54 +0300

    Задача6 выполнена

---
+++ Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа для определения чётного и нечётного числа.
+Console.Clear();
+Console.Write("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+Console.WriteLine($"Данное число {a} - чётное");
+else
+Console.WriteLine($"Данное число {a} - нечётное");
